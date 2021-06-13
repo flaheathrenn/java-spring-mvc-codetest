d5a8cde2b871d760e3fba35916534f279706b96b
Frederic Heath-Renn <frederic@twickets.co.uk>
2021-06-13 15:58:03 +0100

    Add service, implement feature, provide unit test

--- src/main/java/com/example/demo/controllers/CustomersController.java
+++ src/main/java/com/example/demo/controllers/CustomersController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controllers;
 
 import com.example.demo.models.Customer;
+import com.example.demo.services.CustomersService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +12,16 @@ import java.util.List;
 @RestController
 public class CustomersController {
 
+    @Autowired
+    private CustomersService customersService;
+
     /**
      * @param customers a List of Customer objects
      * @return a List of Customer objects containing all the same objects as input, sorted by duetime from oldest to newest
      */
     @PostMapping("/customers/sort")
     public List<Customer> sortAscendingDuetime(@RequestBody List<Customer> customers) {
-        System.out.println("Received " + customers.size() + " customers"); // would normally use a proper logging framework here
+        customersService.sortAscendingDuetime(customers);
         return customers;
     }
 }
---
+++ src/main/java/com/example/demo/services/CustomersService.java
@@ -0,0 +1,20 @@
+package com.example.demo.services;
+
+import com.example.demo.models.Customer;
+import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+
+@Service
+public class CustomersService {
+
+    /**
+     * Sort a list of Customer objects by duetime from oldest to newest.
+     * This method modifies the object passed to it.
+     * @param customers a List of Customers to be sorted
+     */
+    public void sortAscendingDuetime(List<Customer> customers) {
+         customers.sort(Comparator.comparing(Customer::getDuetime));
+    }
+}
