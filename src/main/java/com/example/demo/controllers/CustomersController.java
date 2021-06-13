68f61e6be405efae7e8c68bc72bc19b17e8fd66b
Frederic Heath-Renn <frederic@twickets.co.uk>
2021-06-13 16:22:34 +0100

    Experiment with @Async annotation

--- src/main/java/com/example/demo/controllers/CustomersController.java
+++ src/main/java/com/example/demo/controllers/CustomersController.java
@@ -3,11 +3,14 @@ package com.example.demo.controllers;
 import com.example.demo.models.Customer;
 import com.example.demo.services.CustomersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 public class CustomersController {
@@ -20,8 +23,16 @@ public class CustomersController {
      * @return a List of Customer objects containing all the same objects as input, sorted by duetime from oldest to newest
      */
     @PostMapping("/customers/sort")
-    public List<Customer> sortAscendingDuetime(@RequestBody List<Customer> customers) {
+    @Async
+    public CompletableFuture<List<Customer>> sortAscendingDuetime(@RequestBody List<Customer> customers) {
+        try {
+            if (LocalDateTime.now().getMinute() == 19) {
+                Thread.sleep(100000L); // sleep for 100 seconds
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         customersService.sortAscendingDuetime(customers);
-        return customers;
+        return CompletableFuture.completedFuture(customers);
     }
 }
